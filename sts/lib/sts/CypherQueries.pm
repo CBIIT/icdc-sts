724660e8da9a8b974977c4ccc2bade742ed92115
bensonml <bensonml@nih.gov>
2020-04-14 17:02:06 -0400

    perltidy

--- sts/lib/sts/CypherQueries.pm
+++ sts/lib/sts/CypherQueries.pm
@@ -3,40 +3,41 @@ use base Exporter;
 use strict;
 our @EXPORT;
 
-@EXPORT=qw/%queries/;
+@EXPORT = qw/%queries/;
 
 our %queries = (
 
-	#// query 2 get domains
-	get_valuesets =>
-	'MATCH (v:value_set) return DISTINCT v.id as id, v.url as url',
+    #// query 2 get domains
+    get_valuesets =>
+      'MATCH (v:value_set) return DISTINCT v.id as id, v.url as url',
 
-	get_nodes => <<Q,
-	     MATCH (n:node) 
-	     RETURN DISTINCT n.handle, n.model
+    get_nodes => <<Q,
+        MATCH (n:node) 
+        RETURN DISTINCT n.handle, n.model
 Q
 
-	#// query 3 get properties
-	get_properties => <<Q,
-	     MATCH (p:property {value_domain:"enum"}) -[:has_value_set]-> (v:value_set) 
-	     RETURN DISTINCT p.handle, p.value_domain, p.model, v.id, v.url
+    #// query 3 get properties
+    get_properties => <<Q,
+        MATCH (p:property {value_domain:"enum"}) -[:has_value_set]-> (v:value_set) 
+        RETURN DISTINCT p.handle, p.value_domain, p.model, v.id, v.url
 Q
-    	#// get the version of the database, used in healthcheck that we can query the database
-    	#// but does not require that the database have any data
-    	get_database_version => <<Q,
-        call dbms.components() 
+
+#// get the version of the database, used in healthcheck that we can query the database
+#// but does not require that the database have any data
+    get_database_version => <<Q,
+       	call dbms.components() 
 	yield name, versions, edition unwind versions as version 
 	return name, version, edition;
 Q
 
-	#// node - list
-	nodes_list => <<Q,
+    #// node - list
+    nodes_list => <<Q,
 	MATCH (n:node)
 	RETURN DISTINCT n.handle, n.model;
 Q
 
-	#// node - detail
-	nodes_detail => <<Q,
+    #// node - detail
+    nodes_detail => <<Q,
 	MATCH (n1:node)
 	OPTIONAL MATCH (n1)<-[:has_src]-(r12:relationship)-[:has_dst]->(n2:node)
 	OPTIONAL MATCH (n3)<-[:has_src]-(r31:relationship)-[:has_dst]->(n1:node)
@@ -56,15 +57,15 @@ Q
 			ct.origin_definition, 
 			o.name;
 Q
-	
-	#// property - list
-	properties_list => <<Q,
+
+    #// property - list
+    properties_list => <<Q,
 	MATCH (p:property) 
 	RETURN DISTINCT p.handle as `property-handle`, p.value_domain, p.model, p.is_required ;
 Q
-	
-	#// property - detail
-	properties_list => <<Q,
+
+    #// property - detail
+    properties_list => <<Q,
 	MATCH (p:property)
 	OPTIONAL MATCH (p)-[:has_value_set]->(vs)
 	OPTIONAL MATCH (p)-[:has_concept]->(cp:concept)
@@ -87,15 +88,15 @@ Q
 			r.handle as `relationship-handle`;
 Q
 
-	#// value_set - list
-	value_sets_list => <<Q,
+    #// value_set - list
+    value_sets_list => <<Q,
 	MATCH (vs:value_set)
 	OPTIONAL MATCH (p:property)-[:has_value_set]->(vs)
 	RETURN DISTINCT p.handle as `property-handle`, vs.id as `id`, vs.url as `url`
 Q
 
-	#// value_set - detail
-	value_sets_detail => <<Q,
+    #// value_set - detail
+    value_sets_detail => <<Q,
 	MATCH (vs:value_set)
 	OPTIONAL MATCH (p:property)-[:has_value_set]->(vs)
 	OPTIONAL MATCH (p)-[:has_concept]->(cp:concept)
@@ -111,16 +112,16 @@ Q
 			t.value as `term-value`;
 Q
 
-	#// concept - list
-	concepts_list => <<Q,
+    #// concept - list
+    concepts_list => <<Q,
 	MATCH (c:concept)
 	MATCH (ct:term)-[:represents]->(c)
 	RETURN DISTINCT c.id as `concept-id`, 
 			ct.value as `concept-term-value`; 
 Q
 
-	#// concept - detail
-	concepts_detail => <<Q,
+    #// concept - detail
+    concepts_detail => <<Q,
 	MATCH (c:concept)
 	MATCH (ct:term)-[:represents]->(c)
 	OPTIONAL MATCH (ct)-[:has_origin]->(cto:origin)
